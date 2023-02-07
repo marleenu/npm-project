85b40c610ad2dbe90ccb3dfabc063eb2183d117b
marleenu <77738059+marleenu@users.noreply.github.com>
2023-02-07 22:36:18 +0200

    fix

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.set("view engine", "ejs");
 
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
 app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname));
-
-app.get("/", (req, res) => {
-  res.render("index");
-});
 
 app.listen(port, () => {});
