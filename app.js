ec5ca8a8a1affbffc4fdda24897bae2ac867a1c9
marleenu <77738059+marleenu@users.noreply.github.com>
2023-02-07 21:46:49 +0200

    amplify

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3001;
+const port = 3000;
 
 app.set("view engine", "ejs");
 
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
-    res.render("index")
-})
+  res.render("index");
+});
 
-app.listen(port, ()=>{})
+app.listen(port, () => {});
